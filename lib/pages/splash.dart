56a8f749d4230d13159fbeeaa83ebf543d233ad9
SebastianDion <sebastian.dion02@gmail.com>
2024-11-02 13:41:38 +0700

    first commit

---
+++ lib/pages/splash.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:iot/pages/startPage.dart';
+
+class Splash extends StatelessWidget {
+  const Splash({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const Startpage()),
+        );
+      },
+      child: Scaffold(
+        backgroundColor: const Color(0xFFACD1AF),
+        body: SafeArea(
+          top: true,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Flexible(
+                child: Align(
+                  alignment: const AlignmentDirectional(0, 0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: Image.asset(
+                      'assets/logo.png',
+                      width: 296,
+                      height: 200,
+                      fit: BoxFit.fitWidth,
+                      alignment: const Alignment(0, 0),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/startPage.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:iot/pages/homePage.dart';
+
+class Startpage extends StatelessWidget {
+  const Startpage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        backgroundColor: const Color(0xFFACD1AF),
+        body: SafeArea(
+          top: true,
+          child: Stack(
+            children: [
+              Stack(
+                children: [
+                  Align(
+                    alignment: const AlignmentDirectional(0, 0),
+                    child: Padding(
+                      padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 0, 50),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(8),
+                        child: Image.asset(
+                          'assets/logo2.png',
+                          width: 300,
+                          height: 300,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: const AlignmentDirectional(0, 1),
+                    child: Padding(
+                      padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 0, 300),
+                      child: ElevatedButton(
+                        onPressed: () {
+                           Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const Homepage()),
+        );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xFF29735B),
+                          padding: const EdgeInsets.symmetric(horizontal: 16),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          elevation: 0,
+                        ),
+                        child: const Text(
+                          'NEXT >',
+                          style: TextStyle(
+                            fontFamily: 'Inter Tight',
+                            color: Colors.white,
+                            fontSize: 12,
+                            fontStyle: FontStyle.italic,
+                            fontWeight: FontWeight.w200,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const Align(
+                    alignment:  AlignmentDirectional(0, 0),
+                    child: Padding(
+                      padding:  EdgeInsetsDirectional.fromSTEB(0, 0, 0, 120),
+                      child: Text(
+                        'Welcome To',
+                        style: TextStyle(
+                          fontFamily: 'Inter',
+                          color:  Color(0x6A29735B),
+                          fontSize: 16,
+                          fontWeight: FontWeight.w900,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: const AlignmentDirectional(1.01, 1.42),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(8),
+                      child: Image.asset(
+                        'assets/Ellipse3.png',
+                        width: 313,
+                        height: 289,
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Align(
+                alignment: const AlignmentDirectional(-1.03, -1.34),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: Image.asset(
+                    'assets/Ellipse2.png',
+                    width: 231,
+                    height: 200,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
